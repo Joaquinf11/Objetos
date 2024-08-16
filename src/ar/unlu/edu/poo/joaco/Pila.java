2a387cfe4b9c70c7838ae3f5b3ed6bd577e91a26
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-16 18:08:39 -0300

    adsad

---
+++ src/ar/unlu/edu/poo/joaco/Pila.java
@@ -0,0 +1,33 @@
+package ar.unlu.edu.poo.joaco;
+
+public class Pila {
+    private NodoPila tope;
+    private int cantidaElementos;
+
+    // apilar desapilar tope vacia
+
+    public boolean estaVacia(){
+        return this.cantidaElementos == 0;
+    }
+
+    public void apilar(int valor){
+        NodoPila nuevoNodo= new NodoPila(valor);
+        if (estaVacia()){
+            this.tope= nuevoNodo;
+            this.cantidaElementos++;
+        }
+        else {
+            nuevoNodo.setSiguiente(this.tope);
+            this.tope=nuevoNodo;
+            this.cantidaElementos++;
+        }
+    }
+
+    public int desapilar(){
+        NodoPila nodo=this.tope;
+        this.tope= nodo.getSiguiente();
+        this.cantidaElementos--;
+        return nodo.getValor();
+    }
+
+}
