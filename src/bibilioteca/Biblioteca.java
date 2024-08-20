2c7eb2ea6fee2cdb43673036bc8530e707f3264e
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-20 20:16:34 -0300

    metodo Alta socio
    funcionando

--- src/bibilioteca/Biblioteca.java
+++ src/bibilioteca/Biblioteca.java
@@ -4,31 +4,43 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Biblioteca {
-    private static  ArrayList<Socio> socios;
-    private ArrayList<Autor> autores;
-    private ArrayList <Ejemplar> ejemplares;
+    private static Socio[] socios;
+    private Autor[] autores;
+    private Ejemplar[] ejemplares;
 
     public Biblioteca(){
-        socios= new ArrayList<>();
-        this.autores= new ArrayList<Autor>();
-        this.ejemplares= new ArrayList<Ejemplar>();
+        socios= new Socio[0];
+        this.autores= new Autor[0];
+        this.ejemplares= new Ejemplar[0];
 
     }
 
-    public static ArrayList<Socio> getSocios() {
+    public Socio[] getSocios() {
         return socios;
     }
 
-    public ArrayList<Autor> getAutores() {
+    public Autor[] getAutores() {
         return this.autores;
     }
 
+    public Ejemplar[] getEjemplares(){ return this.ejemplares;}
+
+
     public static void altaSocio(String nombreSocio, int idSocio){
-      socios.add(new Socio(nombreSocio,idSocio));
+
+        Socio[] nuevoArreglo = new Socio[socios.length + 1];
+        System.arraycopy(socios, 0, nuevoArreglo, 0, socios.length);
+        nuevoArreglo[nuevoArreglo.length - 1] = new Socio(nombreSocio,idSocio);
+        socios = nuevoArreglo;
     }
 
+    public void mostrarSocios(){
+        for (Socio socio:socios){
+            System.out.println(socio.getNombreSocio());
+        }
+    }
     public void altaAutor(String nombreAutor, String nacionalidad){
-        autores.add(new Autor(nombreAutor,nacionalidad));
+
     }
 
     public void altaLibro(String nombreAutor,String titulo,int cantidadPaginas){
