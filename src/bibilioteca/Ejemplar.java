feed180a82879773d0f9046ce7d4169bad5816fd
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-23 10:45:11 -0300

    Update Ejemplar.java

--- src/bibilioteca/Ejemplar.java
+++ src/bibilioteca/Ejemplar.java
@@ -6,14 +6,11 @@ import java.util.Objects;
 public class Ejemplar {
     private final Libro libro;
     private boolean disponible;
-    private int ejemplares_prestados;
-    private int ejemplares_disponibles;
+    private  Prestamo prestamo;
 
     public Ejemplar(Libro libro) {
         this.libro = libro;
-        this.ejemplares_prestados=0;
-        this.ejemplares_disponibles=1;
-        this.disponible=false;
+        this.disponible= true;
     }
 
 
@@ -21,41 +18,22 @@ public class Ejemplar {
         return this.libro;
     }
 
-    public int getEjemplares_prestados() {
-        return this.ejemplares_prestados;
-    }
 
-    public int getEjemplares_disponibles() {
-        return this.ejemplares_disponibles;
-    }
 
     public boolean isDisponible() {
         return this.disponible;
     }
 
-    public void setDisponible(){
-        this.disponible= ejemplares_disponibles > 1;
-    }
-
-    public void sumarEjemplar(){
-        this.ejemplares_disponibles++;
-        if (ejemplares_disponibles > 1){
-            this.disponible=true;
-        }
+    public void setDisponible(boolean disponible){
+        this.disponible= disponible;
     }
 
 
-    public void restarEjemplar(){
-        if (getEjemplares_disponibles() > 1){
-            this.ejemplares_disponibles--;
-            this.ejemplares_prestados++;
-            if (getEjemplares_prestados()==1){
-                this.disponible=false;
-            }
-        }
-        else{
-            System.out.println("No se puede prestar libro porque solo queda uno");
-        }
+    public void setPrestamo(Prestamo prestamo){
+        this.prestamo=prestamo;
     }
+
+
+
 
 }
