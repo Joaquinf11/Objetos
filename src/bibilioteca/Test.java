5ed7d67e53e6bb2a07a50966cd522c45dff647ed
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-26 22:37:10 -0300

    comente pa no cambiar todo

--- src/bibilioteca/Test.java
+++ src/bibilioteca/Test.java
@@ -9,27 +9,27 @@ public class Test {
         this.libro_2 = libro_2;
     }
 
-    public String getTitulo_1() {
-        return titulo_1;
-    }
+//    public String getTitulo_1() {
+//        return titulo_1;
+//    }
+//
+//    public String getTitulo_2() {
+//        return titulo_2;
+//    }
 
-    public String getTitulo_2() {
-        return titulo_2;
-    }
-
-    public Libro masCantidadPaginas(){
-        Libro libro_1= Autor.buscarLibro(Biblioteca.getAutores(),getTitulo_1());
-        Libro libro_2= Autor.buscarLibro(Biblioteca.getAutores(),getTitulo_2());
-        if (libro_1.getCantidadPaginas() > libro_2.getCantidadPaginas()){
-            return libro_1;
-        }
-        else{
-            return libro_2;
-        }
-    }
-    public void mostrarTest(){
-        Libro resultado= masCantidadPaginas();
-        System.out.println("El libro con mas paginas es: " + resultado.getTitulo());
-    }
+//    public Libro masCantidadPaginas(){
+//        Libro libro_1= Autor.buscarLibro(Biblioteca.getAutores(),getTitulo_1());
+//        Libro libro_2= Autor.buscarLibro(Biblioteca.getAutores(),getTitulo_2());
+//        if (libro_1.getCantidadPaginas() > libro_2.getCantidadPaginas()){
+//            return libro_1;
+//        }
+//        else{
+//            return libro_2;
+//        }
+//    }
+//    public void mostrarTest(){
+//        Libro resultado= masCantidadPaginas();
+//        System.out.println("El libro con mas paginas es: " + resultado.getTitulo());
+//    }
 }
 
