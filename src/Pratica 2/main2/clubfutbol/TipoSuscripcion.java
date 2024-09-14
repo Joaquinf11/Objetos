5c04e7ffe7a213a10dd9bcee1947aea9f8efba8b
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-09-14 16:23:42 -0300

    Update TipoSuscripcion.java

--- src/Pratica 2/main2/clubfutbol/TipoSuscripcion.java
+++ src/Pratica 2/main2/clubfutbol/TipoSuscripcion.java
@@ -1,6 +1,17 @@
 package clubfutbol;
 
 public enum TipoSuscripcion {
-    BASICA,INTERMEDIA,DESTACADA
-}
+    BASICA,INTERMEDIA,DESTACADA;
 
+
+    public static TipoSuscripcion toTipoSuscripcion(String tipo) {
+        return switch (tipo) {
+            case "Basica" -> TipoSuscripcion.BASICA;
+            case "Intermedia" -> TipoSuscripcion.INTERMEDIA;
+            case "Destacada" -> TipoSuscripcion.DESTACADA;
+            default -> null;
+            // aca prodria haber un exception error de si carga mal la suscripcion que la vuelva a cargar
+        };
+    }
+
+}
