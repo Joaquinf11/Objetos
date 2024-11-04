6971c46206a951fb198dc71ba6ddb47ac794dd95
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-04 19:33:13 -0300

    main interfaces graficas unlu

---
+++ src/Pratica 2/main2/InterfacesGraficas/Main.java
@@ -0,0 +1,10 @@
+package InterfacesGraficas;
+
+import InterfacesGraficas.Vista.VentanaBasica;
+
+public class Main {
+
+    public static void main( String[] args){
+        VentanaBasica v = new VentanaBasica();
+    }
+}
